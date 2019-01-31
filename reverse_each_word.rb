a8cd776cee6498d07b80f618c504411d564297ab
Zachary <zbberman@yahoo.com>
2019-01-31 18:42:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   string_array = []
-  (string.split.each do | letter | 
+  (string.split).each do | letter | 
     string_array << letter
   end 
   string_array.join
